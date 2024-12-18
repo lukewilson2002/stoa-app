c091f1ee22e815f8890392dad02e09d16e099f85
Luke Wilson <thelukaswils@gmail.com>
2024-12-18 17:30:59 -0600

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 export { GET, POST } from "@/lib/auth";
 
 // Checks session expiry and refreshes if necessary
-export { auth as middleware } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+export const middleware = auth;
 
 // export const runtime = "edge";
